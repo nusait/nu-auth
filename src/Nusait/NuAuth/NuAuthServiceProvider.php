969d7d9fb7bdba17a69e5996d32b04b8729b2b8f
Hao Luo <howlowck@gmail.com>
2013-10-24 00:13:08 -0500

    fixed package name

--- src/Nusait/NuAuth/NuAuthServiceProvider.php
+++ src/Nusait/NuAuth/NuAuthServiceProvider.php
@@ -12,7 +12,7 @@ class NuAuthServiceProvider extends ServiceProvider {
 	protected $defer = true;
 
 	public function boot() {
-		$this->package('nuauth/nu-auth');
+		$this->package('nusait/nu-auth');
 		$this->app['auth']->extend('nuauth', function($app) {
 			$autoCreate = $app['config']->get('nu-auth::config');
 		    return new NuAuth($app['hash'], $app['config']['auth.model'], $autoCreate);
